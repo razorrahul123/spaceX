5555faa3957908bf92b7e858ebc30371cdcb61f1
razorrahul123 <rahul.kr.sushant@gmail.com>
2020-08-05 17:23:40 +0530

    Updates

---
+++ precache-manifest.df3fcefdbd2c9e31af096044c69f3910.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfb2186e9c4d1a766b6c75c3af7cdafb",
+    "url": "/spaceX/index.html"
+  },
+  {
+    "revision": "ba1e5a2f1589de0f7e9a",
+    "url": "/spaceX/static/css/main.fa03c23c.chunk.css"
+  },
+  {
+    "revision": "ce8a50664c08299ea936",
+    "url": "/spaceX/static/js/2.902fa1d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spaceX/static/js/2.902fa1d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba1e5a2f1589de0f7e9a",
+    "url": "/spaceX/static/js/main.c73fd5a5.chunk.js"
+  },
+  {
+    "revision": "e940ab3cd99aa93ed5fe",
+    "url": "/spaceX/static/js/runtime-main.46af16b2.js"
+  },
+  {
+    "revision": "1e156b4380a899355bb48291b8316bd6",
+    "url": "/spaceX/static/media/Spinner.1e156b43.svg"
+  }
+]);
